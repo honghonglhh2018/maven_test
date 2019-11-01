ddebfa30f027c62ce4dce7b4bc425eb216110725
Lin <Linhonghong1994@gmail.com>
2019-11-01 14:45:15 +0800

    修改：1:新增输出改动

--- src/main/java/com/sdmc/servlet/ServletDemo.java
+++ src/main/java/com/sdmc/servlet/ServletDemo.java
@@ -10,5 +10,6 @@ public class ServletDemo extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/hello. jsp").forward(req, resp);
+        System.out.println("----");
     }
 }
